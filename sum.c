d2ea7064465a8d32fdd96258a855dd05d8a0ee2d
owaisobaid <105284258+owaisobaid@users.noreply.github.com>
2023-03-18 07:48:38 +0000

    Sat, Mar 18, 2023, 7:48 AM +00:00

--- sum.c
+++ sum.c
@@ -2,9 +2,9 @@
 int main()
 {
     float a,b,sum;
-    printf("enter 2 numbers");
+    printf("enter 2 numbers=");
     scanf("%f %f",&a,&b);
     sum=a+b;
-    printf("sum=%f",sum);
+    printf("sum=%.3f\n",sum);
     return 0;
 }
