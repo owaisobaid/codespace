47d69388bb5caca6cdbd2d69d7193da781955b9a
owaisobaid <105284258+owaisobaid@users.noreply.github.com>
2023-03-18 07:20:22 +0000

    Sat, Mar 18, 2023, 7:20 AM +00:00

--- average.c
+++ average.c
@@ -2,10 +2,10 @@
 int main()
 {
 float s;
-printf("enter marks");
-   /*for(s=0;s<=5;s++)
+printf("enter marks=\n");
+   for(s=0;s<=5;s++)
     {
-        scanf("%d",&s);
-        printf("s1=%d",s);
-    }*/
+        scanf("%f",&s);
+        printf("s1=%f",s);
+    }
 }
