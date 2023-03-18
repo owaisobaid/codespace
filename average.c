3df9267d10124827041b83adb0e1f3339d8d61e3
owaisobaid <105284258+owaisobaid@users.noreply.github.com>
2023-03-18 07:05:16 +0000

    Sat, Mar 18, 2023, 7:05 AM +00:00

--- average.c
+++ average.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    float s1,s2,s3,s4,s5,i;
+    float s,s2,s3,s4,s5,i;
     for(i=0;i<=5;i++)
     {
-        printf("s1=%f",s1);
+        printf("s=%f",s);
     }
 }
