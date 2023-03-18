b7733df90fd6e59ad97a7c13678768471d2eaeee
owaisobaid <105284258+owaisobaid@users.noreply.github.com>
2023-03-18 06:33:32 +0000

    Sat, Mar 18, 2023, 6:33 AM +00:00

---
+++ si.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float p,r,t,si;
+    printf("principal amount=");
+    scanf("%f",&p);
+    printf("rate of interest=");
+    scanf("%f,&r);
+    printf("time period=");
+    scanf("%f",&t);
+    si=p*r/100*t;
+    printf("simple interest=%f",si);
+
+}
